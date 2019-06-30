e3c27bb57a641db37c57622b2fe98b5252f7c4c7
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-06-30 16:05:21 +0200

    Add new global setting constant

---
+++ app/context_processors.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+
+def global_settings(request):
+    # return any necessary values
+    return {
+        'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY
+    }
