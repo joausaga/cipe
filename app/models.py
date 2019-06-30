81ffb36847e264df7bb451247850192272d94ef2
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-06-30 10:23:04 +0200

    Add registration form and map

--- app/models.py
+++ app/models.py
@@ -1,58 +1,5 @@
 from django.db import models
-
-
-SEX = (
-    ('femenino', 'Femenino'),
-    ('masculino', 'Masculino'),
-    ('otro', 'Otro'),
-)
-
-SCIENTIFIC_AREA = (
-    ('agricultura', 'Agricultura'),
-    ('artes', 'Artes'),
-    ('biologia', 'Biología'),
-    ('biotecnologia_agricola', 'Biotecnología Agrícola'),
-    ('biotecnologia_ambiente', 'Biotecnología del Ambiente'),
-    ('biotecnologia_industrial', 'Biotecnología Industrial'),
-    ('biotecnologia_medica', 'Biotecnología Médica'),
-    ('educacion', 'Ciencias de la Educación'),
-    ('ciencias_politicas', 'Ciencias Políticas'),
-    ('ciencias_salud', 'Ciencias de la Salud'),
-    ('comunicaciones', 'Comunicaciones'),
-    ('economia', 'Economía'),
-    ('filosofia', 'Filosofía'),
-    ('fisica', 'Fisica'),
-    ('ganaderia', 'Ganadería'),
-    ('geografia', 'Geografía'),
-    ('historia', 'Historia y Arqueología'),
-    ('informatica', 'Informática'),
-    ('ing_ambiente', 'Ingenieria del Ambiente'),
-    ('ing_civil', 'Ingenieria Civil'),
-    ('ing_electronica', 'Ingenieria Electrónica'),
-    ('ing_materiales', 'Ingenieria de Materiales'),
-    ('ing_mecanica', 'Ingenieria Mecánica'),
-    ('ing_quimica', 'Ingenieria Química'),
-    ('lengua_literatura', 'Lengua y Literatura'),
-    ('leyes', 'Leyes'),
-    ('matematica', 'Matematica'),
-    ('medician_basica', 'Medicina Básica'),
-    ('medician_clinica', 'Medicina Clinica'),
-    ('nano_tecnologia', 'Nano-Tecnología'),
-    ('psicologia', 'Psicología'),
-    ('quimica', 'Quimica'),
-    ('religion', 'Religión'),
-    ('sociologia', 'Sociología'),
-    ('veterinara', 'Veterinaria'),
-    ('otro', 'Otro'),
-)
-
-POSITION = (
-    ('master_academico', 'Estudiante de Máster Académico'),
-    ('doctorando', 'Estudiante de Doctorado'),
-    ('postdoc', 'Post-doc'),
-    ('profesor', 'Profesor'),
-    ('otro', 'Otro'),
-)
+from app.constants import SEX, SCIENTIFIC_AREA, POSITION
 
 
 class Scientist(models.Model):
