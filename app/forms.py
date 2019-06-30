22fcc9b41dbf7dbac3248766001e860a0ee30baa
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-06-30 10:23:04 +0200

    Add registration form and map

--- app/forms.py
+++ app/forms.py
@@ -1,30 +1,69 @@
 from django import forms
-from app.models import Scientist
+from app.constants import SEX, SCIENTIFIC_AREA, POSITION
+
+SEX_EMPTY = [('','Indique su sexo')] + list(SEX)
+SCI_AREA_EMPTY = [('','Indique su área de actuación')] + list(SCIENTIFIC_AREA)
+POSITION_EMPTY = [('','Indique su nivel académico')] + list(POSITION)
 
 
-class ScientistForm(forms.ModelForm):
-    class Meta:
-        model = Scientist
-        fields = [
-            'first_name',
-            'last_name',
-            'sex',
-            'email',
-            'scientific_area',
-            'position',
-            'twitter_handler',
-            'gscholar_profile',
-            'scopus_profile',
-            'orcid'
-        ]
-        labels = {
-            'first_name': 'Nombre',
-            'last_name': 'Apellido',
-            'sex': 'Sexo',
-            'scientific_area': 'Area de Investigación',
-            'position': 'Posición',
-            'twitter_handler': 'Usuario de Twitter (sin @)',
-            'gscholar_profile': 'Perfil de Google Scholar',
-            'scopus_profile': 'Perfil de Scopus',
-            'orcid': 'Orcid'
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField(label='Nombre', widget=forms.TextInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Nombre'
         }
+    ))
+    last_name = forms.CharField(label='Apellido', widget=forms.TextInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Apellido'
+        }
+    ))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Email'
+        }
+    ))
+    sex = forms.ChoiceField(label='Sexo', choices=SEX_EMPTY, required=False, widget=forms.Select(
+        attrs={
+            'class':'form-control'
+        }
+    ))
+    scientific_area = forms.ChoiceField(label='Area de Actuación', choices=SCI_AREA_EMPTY, widget=forms.Select(
+        attrs={
+            'class':'form-control'
+        }
+    ))
+    positions = forms.ChoiceField(label='Nivel Académico', choices=POSITION_EMPTY, widget=forms.Select(
+        attrs={
+            'class':'form-control'
+        }
+    ))
+    twitter_handler = forms.CharField(label='Usuario de Twitter', help_text='sin @', required=False, widget=forms.TextInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Usuario de Twitter'
+        }
+    ))
+    gscholar_profile = forms.CharField(label='Perfil en Google Scholar', required=False, widget=forms.TextInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Perfil en Google Scholar'
+        }
+    ))
+    scopus_profile = forms.CharField(label='Perfil en Scopus', required=False, widget=forms.TextInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Perfil en Scopus'
+        }
+    ))
+    orcid = forms.CharField(label='Perfil Orcid', required=False, widget=forms.TextInput(
+        attrs={
+            'class':'form-control',
+            'placeholder':'Perfil Orcid'
+        }
+    ))
+    location_name = forms.CharField(widget=forms.HiddenInput(), required=False)
+    location_lat = forms.CharField(widget=forms.HiddenInput(), required=False)
+    location_lng = forms.CharField(widget=forms.HiddenInput(), required=False)
--- app/views.py
+++ app/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
-from app.forms import ScientistForm
+from django.http import HttpResponseRedirect
+from app.forms import ScientistForm, RegistrationForm
 
 
 def index(request):
-    return render(request, 'home.html', {})
+    return render(request, 'index.html', {})
 
 
 def create_scientist(request):
-    form = ScientistForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ScientistForm()
+    form = RegistrationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            print(form.cleaned_data)
+            print("Formulario válido!")
+            #form.save()
+            return HttpResponseRedirect('/')
     context = {
         'form': form
     }
-    return render(request, 'create.html', context)
+    return render(request, 'register.html', context)
+
+
+def success_registration(request):
+    return render(request, 'success.html')
+
+
+def map_scientists(request):
+    context = {
+        'lat': 41.389633,
+        'lon': 2.116217
+    }
+    return render(request, 'map.html', context)
