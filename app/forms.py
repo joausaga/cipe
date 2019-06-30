e1f262e430711e65ee4be16f3393bc1354fc0744
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-06-30 16:07:09 +0200

    Update form with new fields

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from app.constants import SEX, SCIENTIFIC_AREA, POSITION
 SEX_EMPTY = [('','Indique su sexo')] + list(SEX)
 SCI_AREA_EMPTY = [('','Indique su área de actuación')] + list(SCIENTIFIC_AREA)
 POSITION_EMPTY = [('','Indique su nivel académico')] + list(POSITION)
+BECAL = [(False, 'Es becario BECAL?'), (False, 'No'), (True, 'Si')]
 
 
 class RegistrationForm(forms.Form):
@@ -30,6 +31,11 @@ class RegistrationForm(forms.Form):
             'class':'form-control'
         }
     ))
+    has_becal_scholarship = forms.ChoiceField(label='Es becario de BECAL?', choices=BECAL, required=False, widget=forms.Select(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
     scientific_area = forms.ChoiceField(label='Area de Actuación', choices=SCI_AREA_EMPTY, widget=forms.Select(
         attrs={
             'class':'form-control'
@@ -46,22 +52,28 @@ class RegistrationForm(forms.Form):
             'placeholder':'Usuario de Twitter'
         }
     ))
-    gscholar_profile = forms.CharField(label='Perfil en Google Scholar', required=False, widget=forms.TextInput(
+    gscholar_profile = forms.URLField(label='Perfil de Google Scholar', required=False, widget=forms.URLInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Perfil en Google Scholar'
+            'placeholder':'Perfil de Google Scholar'
         }
     ))
-    scopus_profile = forms.CharField(label='Perfil en Scopus', required=False, widget=forms.TextInput(
+    scopus_profile = forms.URLField(label='Perfil Scopus', required=False, widget=forms.URLInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Perfil en Scopus'
+            'placeholder':'Perfil Scopus'
         }
     ))
-    orcid = forms.CharField(label='Perfil Orcid', required=False, widget=forms.TextInput(
+    institutional_website = forms.URLField(label='Perfil en web institucional', required=False, widget=forms.URLInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Perfil en web institucional'
+        }
+    ))
+    orcid = forms.CharField(label='Identificador Orcid', required=False, widget=forms.TextInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Perfil Orcid'
+            'placeholder':'Identificador Orcid'
         }
     ))
     location_name = forms.CharField(widget=forms.HiddenInput(), required=False)
