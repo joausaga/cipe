257b65b90aaaf08f5ba70991f7d089ca411c26b9
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-08-11 17:49:38 +0200

    Javascript functions related to the site

---
+++ static/js/cipe.js
@@ -0,0 +1,26 @@
+// Initialize and add the map
+var InforObj = [];
+var centerCords = {
+    lat: 41.389633,
+    lng: 2.116217
+};
+window.onload = function () {
+    initMapHeader();
+};
+function closeInfoWindow() {
+    if (InforObj.length > 0) {
+        /* detach the info-window from the marker */
+        InforObj[0].set("marker", null);
+        /* and close it */
+        InforObj[0].close();
+        /* blank the array */
+        InforObj.length = 0;
+    }
+}
+function initMapHeader() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: centerCords
+    });
+    addMarkers();
+}
