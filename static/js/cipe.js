d5db1d917bf7856565f1600b88bb0c528768f873
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-09-22 23:58:05 +0200

    Move function to javascript file

--- static/js/cipe.js
+++ static/js/cipe.js
@@ -17,10 +17,31 @@ function closeInfoWindow() {
         InforObj.length = 0;
     }
 }
+
+function addMarkers(scientists) {
+    // Create markers
+    //var scientists = {{ scientists|safe }};
+    console.log(scientists);
+    var inst_lat, inst_lng, str_info_window;
+    for (i=0; i < scientists.length; i++) {
+        inst_lat = scientists[i].institution_latitude;
+        inst_lng = scientists[i].institution_longitude;
+        const marker = new google.maps.Marker({position: {lat: inst_lat, lng: inst_lng}, map: map});
+        infoWindowContent = "<div class='text-body text-left'><b>" + scientists[i].name + "</b><br>" + scientists[i].institution_name + "</div>";
+        const infowindow = new google.maps.InfoWindow({
+            content: infoWindowContent
+        });
+        marker.addListener('click', function () {
+            closeInfoWindow();
+            infowindow.open(marker.get('map'), marker);
+            InforObj[0] = infowindow;
+        });
+    }
+}
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
         center: centerCords
     });
-    addMarkers();
 }
