67c69206999c21010b12cb4375bee292490724e1
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-10-12 17:56:47 +0200

    Add filters to the map view

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('registration/', views.registration, name='registration'),
     path('success/', views.success_registration),
     path('map/', views.map_scientists, name='map'),
+    path('map/filter_map/', views.filter_map, name='filter_map')
 ]
