8d82253d1abc85901966919fb6a59058d4c59707
Jorge Saldivar <jorgesaldivar@gmail.com>
2019-12-06 17:33:12 +0100

    Add new fields to the scientist model

--- app/constants.py
+++ app/constants.py
@@ -7,6 +7,59 @@ SEX = (
 
 # Taken from the OECD's Frascati Manual
 # https://read.oecd-ilibrary.org/science-and-technology/frascati-manual-2015_9789264239012-en#.XeKZE-dKjRY
+MAIN_SCIENTIFIC_AREA = {
+    'ciencias_naturales' : [
+        'matematica', 'ciencias_informacion',
+        'fisica', 'quimica',
+        'ciencias_ambiente', 'biologia',
+        'otras_naturales'
+    ],
+    'ingenieria': [
+        'ing_civil', 'ing_electronica',
+        'informatica', 'ing_mecanica',
+        'ing_quimica', 'ing_materiales',
+        'ing_medica', 'ing_ambiente',
+        'biotecnologia_ambiente',
+        'biotecnologia_industrial',
+        'nano_tecnologia',
+        'otras_ingenierias'
+    ],
+    'ciencias_medicas': [
+        'medicina_basica', 'medicina_clinica',
+        'ciencias_salud', 'biotecnologia_medica',
+        'otras_medica'
+    ],
+    'ciencias_agricolas': [
+        'agricultura', 'ciencias_animal',
+        'veterinaria', 'biotecnologia_agricola',
+        'otras_agricola'
+    ],
+    'ciencias_sociales': [
+        'psicologia', 'economia',
+        'educacion', 'sociologia',
+        'leyes', 'ciencias_politicas',
+        'geografia', 'comunicaciones',
+        'otras_sociales'
+    ],
+    'humanidades_artes': [
+        'historia', 'lengua_literatura',
+        'filosofia', 'arte',
+        'otras_humanas'
+    ]
+}
+
+
+FIRST_CAT_SCIENTIFIC_AREA = (
+    ('ciencias_naturales', 'Ciencias Naturales'),
+    ('ingenieria', 'Ingeniería y Tecnología'),
+    ('ciencias_medicas', 'Ciencias Médicas y de la Salud'),
+    ('ciencias_agricolas', 'Ciencias Agrícolas y Veterinarias'),
+    ('ciencias_sociales', 'Ciencias Sociales'),
+    ('humanidades_artes', 'Humanidades y Artes'),
+    ('', ''),
+)
+
+
 SCIENTIFIC_AREA = (
     ('agricultura', 'Agricultura, Silvicultura y Pesca'),
     ('arte', 'Artes'),
@@ -46,7 +99,7 @@ SCIENTIFIC_AREA = (
     ('sociologia', 'Sociología'),
     ('veterinaria', 'Veterinaria'),
     ('otras_agricola', 'Otras ciencias agricolas'),
-    ('otras_agricola', 'Otras ciencias humanas'),
+    ('otras_humanas', 'Otras ciencias humanas'),
     ('otras_naturales', 'Otras ciencias naturales'),
     ('otras_medica', 'Otras ciencias médicas'),
     ('otras_ingenierias', 'Otras ingenierías y tecnologías'),
@@ -66,5 +119,6 @@ COMMUNICATION_CHANNELS = (
     ('slack', 'Espacio de Trabajo en Slack'),
     ('telegram', 'Grupo de Telegram'),
     ('lista_correo', 'Lista de Correo'),
-    ('facebook', 'Grupo de Facebook')
+    ('facebook', 'Grupo de Facebook'),
+    ('otro', '')
 )
